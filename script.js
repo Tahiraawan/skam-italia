270daa9679439f79517b2a2433792c48eb1a5a08
tahiramalik <85984818+Tahiraawan@users.noreply.github.com>
2023-07-07 18:05:18 +0500

    added pages

---
+++ script.js
@@ -0,0 +1,43 @@
+var previousScroll = window.pageYOffset;
+
+window.addEventListener('scroll', function() {
+  var currentScroll = window.pageYOffset;
+  
+  if (currentScroll > previousScroll) {
+    // Scrolling down
+    document.querySelector('header').classList.add('hide');
+  } else {
+    // Scrolling up
+    document.querySelector('header').classList.remove('hide');
+  }
+  
+  previousScroll = currentScroll;
+});
+
+// for carousal
+let flag = 0;
+
+document.addEventListener("DOMContentLoaded", function () {
+  slideShow(flag);
+});
+
+function controller(x) {
+  flag += x;
+  slideShow(flag);
+}
+
+function slideShow(num) {
+  let slides = document.getElementsByClassName("slide");
+  if (num >= slides.length) {
+    flag = 0;
+    num = 0;
+  }
+  if (num < 0) {
+    flag = slides.length - 1;
+    num = slides.length - 1;
+  }
+  for (let y of slides) {
+    y.style.display = "none";
+  }
+  slides[num].style.display = "block";
+}
